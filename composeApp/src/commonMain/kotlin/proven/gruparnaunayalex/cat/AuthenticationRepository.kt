9cf4c6401c9e0c6b8697e2fd3290eb5dfd4b3f01
nayrariosaguilar <nayrios2004@gmail.com>
2025-03-05 00:05:42 +0100

    clean

--- composeApp/src/commonMain/kotlin/proven/gruparnaunayalex/cat/AuthenticationRepository.kt
+++
@@ -1,8 +0,0 @@
-package proven.gruparnaunayalex.cat
-
-interface AuthenticationRepository {
-    suspend fun signIn(email: String, password: String): Boolean
-    suspend fun signUp(email: String, password: String): Boolean
-    suspend fun signInWithGoogle(): Boolean
-
-}
