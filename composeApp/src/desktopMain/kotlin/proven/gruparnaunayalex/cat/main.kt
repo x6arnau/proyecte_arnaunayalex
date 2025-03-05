108c4acd6c97bf0a6da93b6da27594e38a6e27b5
nayrariosaguilar <nayrios2004@gmail.com>
2025-03-05 21:08:45 +0100

    dwd

--- composeApp/src/desktopMain/kotlin/proven/gruparnaunayalex/cat/main.kt
+++ composeApp/src/desktopMain/kotlin/proven/gruparnaunayalex/cat/main.kt
@@ -1,6 +1,9 @@
 package proven.gruparnaunayalex.cat
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import kotlin.io.path.Path
+import kotlin.io.path.deleteIfExists
 
 fun main() = application {
     //version libreria avanmzada de sqlite para no tener que hacer el driver
